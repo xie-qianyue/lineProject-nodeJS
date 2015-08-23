8b4bd5ccb2d82e7df722241c89deba2f71492ffe
xie-qianyue <xie.qianyue@gmail.com>
2015-08-23 23:21:55 +0200

    correct gulp error

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var mainBowerFiles = require('main-bower-files');
 gulp.task('bower', function(){
 
 	var jsFilter = gulpFilter('*.js');
-	var dest = '/public/scripts/externe'
+	var dest = './public/scripts/externe'
 
 	return gulp.src(mainBowerFiles())
 		.pipe(jsFilter)
