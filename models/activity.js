2ddba56ae1a0f1ef0575fc8338005e4421845da6
xie-qianyue <xie.qianyue@gmail.com>
2015-10-09 22:01:17 +0200

    add models

---
+++ models/activity.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+// Define the shcema.
+var activitySchema = new mongoose.Schema({
+	name: String,
+	start_at: Date,
+	last_modified: Date
+});
+
+// Define model with a pre-exist collection 'todo'.
+var activity = mongoose.model('Activity', activitySchema);
