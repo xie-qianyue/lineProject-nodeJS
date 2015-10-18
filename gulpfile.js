7bf8e8438d41be0489f518eaf03eecbd5509a135
XIE Qianyue <xie.qianyue@gmail.com>
2015-10-18 22:18:50 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulpFilter = require('gulp-filter');
 var mainBowerFiles = require('main-bower-files');
 var beautify = require('gulp-beautify');
 
-gulp.task('bower-js', function() {
+gulp.task('movejs', function() {
 
     var jsFilter = gulpFilter('*.js');
     var jsDest = './public/scripts/externe';
@@ -13,7 +13,7 @@ gulp.task('bower-js', function() {
         .pipe(gulp.dest(jsDest));
 });
 
-gulp.task('styles', function() {
+gulp.task('movecss', function() {
 
     // var cssFilter = gulpFilter('*.css');
     var cssDest = './public/css'
@@ -33,5 +33,5 @@ gulp.task('beautify-js', function() {
 });
 
 gulp.task('all', function() {
-    gulp.start('beautify-js', 'bower-js', 'styles');
+    gulp.start('movejs', 'movecss');
 });
