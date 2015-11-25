5aad731fa1d3f21e56d3842a875709a8fb9f8101
xie-qianyue <xie.qianyue@gmail.com>
2015-11-25 17:49:02 +0100

    replace userService by  in createUser, change success to then

--- public/scripts/controllers/signupController.js
+++ public/scripts/controllers/signupController.js
@@ -1,6 +1,9 @@
 'use strict';
 
-app.controller('signupCtrl', function($scope, Auth) {
+app.controller('signupCtrl', function($scope, Auth, $rootScope) {
+
+    $rootScope.message = "";
+
     $scope.register = function() {
         Auth.createUser({
             email: $scope.user.email,
