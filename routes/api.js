a1b81ff020583dcb41f96865d71e54625b7b2099
Qianyue XIE <xie.qianyue@gmail.com>
2015-11-26 22:23:41 +0100

    add activities routes

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,48 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var api = express.Router();
-var actvity = require('./../models/activity.js');
+var actvity = require('./../models/activity');
 var User = require('../models/user');
 
-api.post('/addActivity'), function (req, res) {
+api.post('/addActivity', function (req, res) {
 
-	var userActivity = req.body.newActivity;
-	var userId = User.findByEmail(activity.userEmail, function(err, user){
+	var userActivity = req.body;
+
+	User.find({'local.email' : userActivity.userEmail}, function(err, user){
+		if(err) {
+			res.statut(500).send('Error DB : cant find user.')
+		}
+
 		var newActvity = new actvity();
-		// TODO
-	})
+		newActvity.user_id = user._id;
+		newActvity.name = userActivity.name;
+		newActvity.start_at = new Date();
+		newActvity.last_modified = new Date();
+		
+		newActvity.save(function(err){
+			if(err) {
+				res.statut(500).send('Error DB : cant save activity');
+			}
+			res.send(userActivity);
+		});
+	});
+});
 
+api.get('/getActivities', function(req, res) {
+
+	User.find({'local.email' : userActivity.userEmail}, function(err, user){
+
+		if(err) {
+			res.statut(500).send('Error DB : cant find user.')
+		}
+
+		actvity.find({'user_id' : user._id}, function(err, activities){
+			if(err) {
+				res.statut(500).send('Error DB : cant find activities');
+			}
+			res.send(activities);
+		});
+	});
 	
 });
 
